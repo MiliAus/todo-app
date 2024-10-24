96cb4c3f7ca99c015641e27487f7736b00dfd9d6
user user <miliaus2014@gmail.com>
2024-10-24 22:06:05 +0000

    added adding

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -6,8 +6,10 @@ import './styles/App.css';
 
 
 export default function App() {
-  
-    const [item, addItem] = useState('');
+    let initialValue = <i>Wow, so empty...you must be having a chill day.</i>
+    const [item, addItem] = useState([initialValue]);
+    const [counter, updateCount] = useState(0);
+
     function handleItem(e){
       e.preventDefault();
     
@@ -15,8 +17,22 @@ export default function App() {
       const form = e.target;
       const formData = new FormData(form);
 
-      const formJson = Object.fromEntries(formData.entries()).myInput;
-      addItem(<Table value={formJson} />)
+      const formJson = Object.fromEntries(formData.entries()).myInput.toString();
+      
+      
+        let row = <Table value={formJson} />
+
+        if (counter === 0){
+          
+          addItem(() => [row]);
+          updateCount(counter+1);
+        }
+        else{
+          console.log(item.length)
+          // using the spread operator (...) we can copy all elements
+          addItem(prevItem => [row, ...prevItem] );
+        }
+      
     }
   return (
     <>
@@ -27,12 +43,12 @@ export default function App() {
         <div>
           <form method='post' onSubmit={handleItem}>
           <label>
-            <input name="myInput" className="myInput" defaultValue="Some initial value" />
+            <input name="myInput" className="myInput" placeholder="What is your first task?" />
           </label>
           <button type="submit" className='addbutton'>Add</button>
           </form>
           <h2 className='roboto-bold'>Tasks</h2>
-          <i>Wow, so empty...</i>
+          {item}
         </div>
           
       </div>
--- todo-app/src/components/Table.tsx
+++ todo-app/src/components/Table.tsx
@@ -4,11 +4,13 @@ import '../styles/Table.css';
 
 export default function Table({value}) {
 
+    
+
     return (
         <table>
             <tr>
                 <td>{value} </td>
-                <span onClick={()=>{console.log("hello")}}className='material-symbols-outlined'>delete</span>
+                <span onClick={()=>{"remove(this)"}}className='material-symbols-outlined'>delete</span>
             </tr>
         </table>
     );
